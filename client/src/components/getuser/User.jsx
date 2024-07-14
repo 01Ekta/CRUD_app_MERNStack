61bfee3a1d38bcb52c2da03effca404dc5242630
Ekta_Gupta <108565384+01Ekta@users.noreply.github.com>
2024-07-15 01:15:40 +0530

    Update User.jsx

--- client/src/components/getuser/User.jsx
+++ client/src/components/getuser/User.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import toast from 'react-hot-toast';
 import "./user.css";
 import { Link } from 'react-router-dom'
 
@@ -10,12 +11,25 @@ const User = () => {
     useEffect(()=>{
 
         const fetchData = async()=>{
-            const response = await axios.get("http://localhost:8000/api/getall")
-        }
+            const response = await axios.get("http://localhost:8000/api/getall");
+            setUsers(response.data);
+        };
 
         fetchData();
     },[])
 
+        const deleteUser = async(userId) => {
+        await axios.delete(`http://localhost:8000/api/delete/${userId}`)
+        .then((response)=>{
+            setUsers((prevUser)=> prevUser.filter((user)=> user._id !== userId))
+            // console.log(response)
+            toast.success(response.data.msg, {position: 'top-right'})
+        })
+        .catch((error) =>{
+            console.log(error);
+        })
+    }
+ 
     return (
         <div className="userTable">
             <Link to={"/add"}>Add User</Link>
@@ -30,14 +44,14 @@ const User = () => {
                 </thead>
                 <tbody>
                     {
-                        users.map((user._id)=>{
+                        users.map((user, index)=>{
                             return(
                             <tr key={user._id}>
                                 <td>{index + 1}</td>
                                 <td>{user.fname} {user.lname)</td>
                                 <td>{user.email}</td>
                                 <td className='actionButtons'>
-                                    <button><i className="fa-solid fa-trash"></i></button>
+                                    <button onClick={(>=> dleteUser(user._id)><i className="fa-solid fa-trash"></i></button>
                                     <Link to={`/edit/`+user._id}><i className="fa-solid fa-pen-to-square"></i></Link>
                                 </td>
                             </tr>
