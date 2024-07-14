82a69eab7fd4a0aa2cafbb3ce89f58070a76d46b
Ekta_Gupta <108565384+01Ekta@users.noreply.github.com>
2024-07-15 01:10:39 +0530

    Update Add.jsx

--- client/src/components/adduser/Add.jsx
+++ client/src/components/adduser/Add.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import "./add.css";
+import toast from 'react-hot-toast';
 
 const Add = () => {
 
@@ -9,10 +10,11 @@ const Add = () => {
         fname:"",
         lname:"",
         email:"",
-        password:""
+        password:"",
     }
 
     const [user, setUser] = useState(users);
+    const navigate = useNavigate();
 
     const inputHandler = (e) => {
         const {name, value} = e.target;
@@ -23,40 +25,31 @@ const Add = () => {
         e.preventDefault();
         await axios.post("localhost:8000/api/create", user)
         .then((response)=>{
-            console.log(response)
+            toast.success(response.data.msg, {position:"top-right"});
+            navigate("/");
         }).catch(error => console.log(error))
-    }
-    
-    const submitForm = async(e) => {
-        e.preventDefault();
-        await axios.post("http://localhost:8000/api/create", user)
-        .then((response)=>{
-            toast.success(response.data.msg, {position:"top-right"})
-            Navigate("/")
-        })
-        .catch(error => console.log(error))
-    }
+    };
 
     return (
         <div className='addUser'>
             <Link to={"/"}>Back</Link>
             <h3>Add new user</h3>
-            <form className="addUserForm">
+            <form className="addUserForm" onSubmit={submitForm}>
                 <div className="inputGroup">
                     <label htmlFor="fname">First name</label>
-                    <input type="text" id="fname" name="fname" autoComplete='off' placeholder='First name' />
+                    <input type="text" id="fname" name="fname" value={user.fname} onChange={inputHandler} autoComplete='off' placeholder='First name' />
                 </div>
                 <div className="inputGroup">
                     <label htmlFor="lname">Last name</label>
-                    <input type="text" id="lname" name="lname" autoComplete='off' placeholder='Last name' />
+                    <input type="text" id="lname" name="lname" value={user.lname} onChange={inputHandler} autoComplete='off' placeholder='Last name' />
                 </div>
                 <div className="inputGroup">
                     <label htmlFor="fname">Email</label>
-                    <input type="text" id="email" name="email" autoComplete='off' placeholder='Email' />
+                    <input type="text" id="email" name="email" value={user.email} onChange={inputHandler} autoComplete='off' placeholder='Email' />
                 </div>
                 <div className="inputGroup">
                     <label htmlFor="fname">Password</label>
-                    <input type="password" id="password" name="password" autoComplete='off' placeholder='Enter your Password' />
+                    <input type="password" id="password" name="password" value={user.password} onChange={inputHandler} autoComplete='off' placeholder='Enter your Password' />
                 </div>
                 <div className="inputGroup">
                     <button type="submit">ADD USER</button>
